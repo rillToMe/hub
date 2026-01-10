f326acee93f86d05fe071f1d772674d6c394e61b
Rahmat Aditya <rahmataditya2817@gmail.com>
2026-01-10 10:31:11 +0700

    Add files via upload

--- admin/src/js/login.js
+++ admin/src/js/login.js
@@ -36,7 +36,7 @@ const AuthStorage = {
 };
 
 if (AuthStorage.isTokenValid()) {
-    window.location.href = "/admin/dashbord.html";
+    window.location.href = "/admin/daskboard.html";
 }
 
 loginForm.addEventListener('submit', async (e) => {
@@ -70,7 +70,7 @@ loginForm.addEventListener('submit', async (e) => {
             btnText.innerText = "Success!";
             
             setTimeout(() => {
-                window.location.href = "/admin/dashbord.html";
+                window.location.href = "/admin/daskboard.html";
             }, 500);
         } else {
             throw new Error(data.message || 'Login failed');
